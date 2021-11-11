1c5c705c9e26ae3862b61cb5431d2670a4719fe7
David <unbekannt.ghost@gmail.com>
2021-11-11 09:35:01 +0100

    add showStudentOf

--- src/app/classes/class-list/class-list.component.ts
+++ src/app/classes/class-list/class-list.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data-service.service';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../services/data-service.service';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-class-list',
-  templateUrl: './class-list.component.html',
-  styleUrls: ['./class-list.component.css'],
+    selector: 'app-class-list',
+    templateUrl: './class-list.component.html',
+    styleUrls: ['./class-list.component.css'],
 })
 export class ClassListComponent implements OnInit {
-  classNames: Array<string>;
+    classNames: Array<string>;
 
-  constructor(private dataService: DataService) {}
+    constructor(private dataService: DataService, private router: Router) {
+    }
 
-  ngOnInit(): void {
-    this.classNames = this.dataService.getClassNames();
-  }
+    ngOnInit(): void {
+        this.classNames = this.dataService.getClassNames();
+    }
+
+    showStudentOf(classname): void {
+        this.router.navigate(['class-detail', classname]);
+    }
 }
