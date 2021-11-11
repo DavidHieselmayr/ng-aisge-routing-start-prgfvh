0a05d60b13e3a25e3be2eebbf5fcbf2af6c93088
David <unbekannt.ghost@gmail.com>
2021-11-11 09:29:19 +0100

    add new route

--- src/app/classes/class-detail/class-detail.component.ts
+++ src/app/classes/class-detail/class-detail.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Student } from '../../model/student.model';
-import { DataService } from '../../services/data-service.service';
+import {Component, OnInit} from '@angular/core';
+import {Student} from '../../model/student.model';
+import {DataService} from '../../services/data-service.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-class-detail',
-  templateUrl: './class-detail.component.html',
-  styleUrls: ['./class-detail.component.css'],
+    selector: 'app-class-detail',
+    templateUrl: './class-detail.component.html',
+    styleUrls: ['./class-detail.component.css'],
 })
 export class ClassDetailComponent implements OnInit {
-  className = '4AHITM';
-  students: Array<Student>;
+    className = 'defaultClass';
+    students: Array<Student>;
 
-  constructor(private dataService: DataService) {}
+    constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-    this.students = this.dataService.getStudentsOfClass(this.className);
-  }
+    ngOnInit(): void {
+        this.className = this.route.snapshot.params['classname'];
+        this.students = this.dataService.getStudentsOfClass(this.className);
+    }
 }
