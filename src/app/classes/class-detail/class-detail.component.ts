44a14ca31fd98070aee1a150e88024ad093ca5a1
David <unbekannt.ghost@gmail.com>
2021-11-18 08:28:33 +0100

    add Observer pattern

--- src/app/classes/class-detail/class-detail.component.ts
+++ src/app/classes/class-detail/class-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Student} from '../../model/student.model';
 import {DataService} from '../../services/data-service.service';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
     selector: 'app-class-detail',
@@ -18,5 +18,10 @@ export class ClassDetailComponent implements OnInit {
     ngOnInit(): void {
         this.className = this.route.snapshot.params['classname'];
         this.students = this.dataService.getStudentsOfClass(this.className);
+
+        this.route.params.subscribe((params: Params)=> {
+            this.className = params['classname'];
+            this.students = this.dataService.getStudentsOfClass(this.className);
+        })
     }
 }
